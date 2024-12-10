a88064af4ad71f0eff85605e3ee225a8a46e14f7
Anthony Datu <anthony.datu@cloudinary.com>
2024-12-10 11:51:11 -0800

    implement cloudinary/url-gen into custom element

--- cld-shoppable-image.js
+++ cld-shoppable-image.js
@@ -1,7 +1,60 @@
+//use parcel: npx parcel index.html
+
+// Import the Cloudinary class.
+import {Cloudinary} from "@cloudinary/url-gen";
+import {scale} from "@cloudinary/url-gen/actions/resize";
+import {outline, cartoonify} from "@cloudinary/url-gen/actions/effect";
+import {max} from "@cloudinary/url-gen/actions/roundCorners";
+import {outer} from "@cloudinary/url-gen/qualifiers/outlineMode";
+
+
+import {
+  lazyload,
+  responsive,
+  //accessibility,
+  //placeholder,
+  HtmlImageLayer
+} from "@cloudinary/html";
+
+
+
 class CldShoppableImage extends HTMLElement {
 	constructor() {
 		super();
-	}
+
+        // Create a Cloudinary instance and set your cloud name.
+        const cld = new Cloudinary({
+            cloud: {
+                cloudName: 'demo'
+              }
+        });
+
+        // cld.image returns a CloudinaryImage with the configuration set.
+        const myImage = cld.image('sample'); // sample is the public ID of the image.
+
+        myImage
+          .effect(cartoonify())
+          .roundCorners(max())
+          .effect(outline().mode(outer()).width(100).color('lightblue'))
+          .backgroundColor('lightblue')
+          .resize(scale().height(300));
+
+        // This returns: https://res.cloudinary.com/demo/image/upload/sample
+        const myURL = myImage.toURL();
+        console.log("url-gen:", myURL);
+
+
+        // Render the images in an 'img' element.
+        const imgTag = document.createElement("img");
+        //document.body.appendChild(imgTag);
+        imgTag.height = 500;
+        imgTag.src = myURL;
+
+
+        var cld_shoppable = document.getElementsByTagName("cld-shoppable-image");
+        cld_shoppable[0].appendChild(imgTag);
+    }
+
 	connectedCallback() {
 		console.log("Custom element added to page.");
 	}
