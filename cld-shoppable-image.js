5763b6dc4d9238fd3a690c43c3d82a598540cf16
eportis-cloudinary <eric@cloudinary.com>
2024-12-13 06:44:36 -0800

    Parse <cld-product>s and create DOM from them
    Don't fake things anymore!

--- cld-shoppable-image.js
+++ cld-shoppable-image.js
@@ -1,107 +1,149 @@
-//use parcel: npx parcel index.html
-
-import {html, render} from 'lit-html';
-import {LitElement} from 'lit-element';
+import { html, render } from 'lit-html';
+import { LitElement } from 'lit-element';
 
 export class CldShoppableImage extends LitElement {
-    createRenderRoot() {
-  return this;
-}
-    render(){
-        const img = this.querySelector('img');
-        return html`
-        
+
+	createRenderRoot() {
+		return this;
+	}
+
+	parseProducts() {
+		const productEls = [ ...this.querySelectorAll( 'cld-product' ) ];
+		return productEls.map( p => ({
+			name: p.getAttribute( 'name' ),
+			href: p.getAttribute( 'href' ),
+			thumb: p.getAttribute( 'thumb' ),
+			category: p.getAttribute( 'category' ),
+			x: parseFloat( p.getAttribute( 'x' ) ) / 100,
+			y: parseFloat( p.getAttribute( 'y' ) ) / 100
+		}) );
+	}
+
+	productListItems( parsedProducts, id ) {
+		return parsedProducts.map( ( p, index ) => html`
+<li class="product" id="${ id }_product_${ index }">
+	<a href="${ p.href }">
+		${ p.thumb ? html`
+		<img
+			class="productThumb"
+			src="${ p.thumb }">
+		` : ''
+		}
+		<div class="productText">
+			${ p.category ? html`<p class=productCategory>${ p.category }</p>` : '' }
+			${ p.name ? html`<h2 class=productName>${ p.name }</h2>` : '' }
+		</div>
+	</a>
+</li>
+`
+		);
+	}
+
+	productHotspots( parsedProducts, id ) {
+		return parsedProducts.map( ( p, index ) => html`
+	<button class="hotspot" id="${ id }_hotspot_${ index }" style="--x: ${ p.x }; --y: ${ p.y };" popovertarget="${ id }_products"></button>
+`
+		);
+	}
+
+	render() {
+
+		const img = this.querySelector( 'img' );
+
+		let id;
+		if ( img.hasAttribute( 'id' ) ) {
+			id = img.getAttribute( 'id' );
+		} else {
+			id = self.crypto.randomUUID();
+			img.setAttribute( 'id', id );
+		}
+
+		const parsedProducts = this.parseProducts();
+
+		// todo should the styles go in here?? repeated with every instance?
+		return html`
 <style>
 @layer base {
-  cld-shoppable-image { display: contents; }
-cld-shoppable-image .photo {
-  anchor-name: --image;
-}
-cld-shoppable-image .productsButton {
-  position: absolute;
-  position-anchor: --image;
-  inset-block-start: anchor(start);
-  margin: 0.25em;
-  anchor-name: --button;
-}
 
-cld-shoppable-image .productsList[popover] {
-  position: absolute;
-  position-anchor: --button;
-  inset-block-start: anchor(end);
-  inset-inline-start: anchor(start);
-  margin: 0;
-  padding: 0;
-}
+	cld-shoppable-image { display: contents; }
 
-cld-shoppable-image .productsButtonText:before { content: "Show "; }
-cld-shoppable-image:has([popover]:popover-open) button[popovertarget] .productsButtonText:before { content: "Hide " }
+	cld-shoppable-image .photo {
+		anchor-name: --image;
+	}
 
-cld-shoppable-image .product a {
-  display: grid;
-  grid-template-columns: 0.33fr 1fr;
-  margin: 0;
-  padding: 0;
-  border: none;
-}
-cld-shoppable-image .productThumb {
-  width: 100%;
-}
-  cld-shoppable-image .hotspot {
-    position: absolute;
-    position-anchor: --image;
-    --diameter: 2rem;
-    inset-inline-start: calc( (anchor(end) * var(--x)) - (var(--diameter) / 2) );
-    inset-block-start: calc( (anchor(end) * var(--y)) - (var(--diameter) / 2) );
-    width: var(--diameter);
-    aspect-ratio: 1;
-    border-radius: 100%;
-    visibility: hidden;
-    cursor: pointer;
-  }
-  cld-shoppable-image:hover .hotspot {
-    visibility: visible;
-  }
+	cld-shoppable-image .productsButton {
+		position: absolute;
+		position-anchor: --image;
+		inset-block-start: anchor(start);
+		margin: 0.25em;
+		anchor-name: --button;
+	}
+
+	cld-shoppable-image .productsList[popover] {
+		position: absolute;
+		position-anchor: --button;
+		inset-block-start: anchor(end);
+		inset-inline-start: anchor(start);
+		margin: 0;
+		padding: 0;
+	}
+
+	cld-shoppable-image .productsButtonText:before { content: "Show "; }
+	cld-shoppable-image:has([popover]:popover-open) button[popovertarget] .productsButtonText:before { content: "Hide " }
+
+	cld-shoppable-image .product a {
+		display: grid;
+		grid-template-columns: 0.33fr 1fr;
+		margin: 0;
+		padding: 0;
+		border: none;
+	}
+
+	cld-shoppable-image .productThumb {
+		width: 100%;
+	}
+
+	cld-shoppable-image .hotspot {
+		position: absolute;
+		position-anchor: --image;
+		--diameter: 2rem;
+		inset-inline-start: calc( (anchor(end) * var(--x)) - (var(--diameter) / 2) );
+		inset-block-start: calc( (anchor(end) * var(--y)) - (var(--diameter) / 2) );
+		width: var(--diameter);
+		aspect-ratio: 1;
+		border-radius: 100%;
+		visibility: hidden;
+		cursor: pointer;
+	}
+
+	cld-shoppable-image:hover .hotspot {
+		visibility: visible;
+	}
+
 }
 </style>
-        
+
 
 <button
-  class="productsButton"
-  popovertarget=photo1234_products
+	class="productsButton"
+	popovertarget="${ id }_products"
 >
-    <span class="productsButtonText">Products</span>
+	<span class="productsButtonText">Products</span>
 </button>
+
 <ul
-  class="productsList"
-  popover
-  id=photo1234_products
+	class="productsList"
+	popover
+	id="${ id }_products"
 >
-  <li class="product" id="product2">
-    <a href="#">
-    <img class="productThumb" src="https://assets.thenorthface.com/image/upload/q_auto,f_auto/v1725637817/summit-pro-120-crew">
-    <div class="productText">
-      <p class=productCategory>Base Layer</p>
-      <h2 class=productName>Summit Pro 120 Crew</h2>
-    </div>
-    </a>
-  </li>
-  <li class="product" id="product1">
-    <a href="#">
-    <img class="productThumb" src="https://assets.thenorthface.com/image/upload/q_auto,f_auto/v1725637819/summit-series-goretex-bibs">
-    <div class="productText">
-      <p class=productCategory>Shell Layer</p>
-      <h2 class=productName>Summit Series Verbier Bibs</h2>
-    </div>
-    </a>
-  </li>
+${ this.productListItems( parsedProducts, id ) }
 </ul>
-  <button class="hotspot" id="product2_hotspot" style="--x: 0.55; --y: 0.35;" popovertarget=photo1234_products></button>
-  <button class="hotspot" id="product1_hotspot" style="--x: 0.5; --y: 0.75;" popovertarget=photo1234_products></button>
-        `;
-    }
+
+${ this.productHotspots( parsedProducts, id ) }
+`;
+
+	}
 
 }
-
 
 customElements.define('cld-shoppable-image', CldShoppableImage);
